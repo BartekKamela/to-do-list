6f074c8ebd333fb7b403e0b03376ae7ea8df1473
Bartlomiej Kamela <bartek@lubnet.pl>
2023-02-26 20:32:52 +0100

    toggle task done

--- js/script.js
+++ js/script.js
@@ -10,6 +10,24 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        
+        render();
+    };
+
+    const removeTask =(index) => {
+        tasks.splice(index, 1);
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
     const render = () => {
         let htmlString = "";
 
@@ -17,20 +35,30 @@
             htmlString += `
                 <li
                     ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+                    <button class="js-done">zrobione?</button>
+                    <button class="js-remove">usuń</button>
                     ${task.content}
                 </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
         });
-        
-        render();
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
     };
 
     const onFormSubmit = (event) => {
