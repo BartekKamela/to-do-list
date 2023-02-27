5587541f77b2ccce022368e982fbfdd897eff397
Bartlomiej Kamela <bartek@lubnet.pl>
2023-02-27 19:53:43 +0100

    added html layout elements

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
             done: true,
         },
     ];
-
+    
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
@@ -51,11 +51,11 @@
 
         for(const task of tasks) {
             htmlString += `
-                <li
-                    ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+                <li 
+                    class="list__item ${task.done ? "list__item--done" : ""}">
                     <button class="js-done">zrobione?</button>
-                    <button class="js-remove">usuń</button>
                     ${task.content}
+                    <button class="js-remove">usuń</button>
                 </li>
             `;
         }
